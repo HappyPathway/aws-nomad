dd66243c473716612eb8704d141edb25056ae6b6
David Arnold <dja+djaboxx@sonic.net>
2018-10-02 22:14:20 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,20 +2,26 @@
 // Modules
 variable "organization" {}
 variable "cluster_name" {}
+variable "hashistack_ami" {
+  default = "ami-0feeb6ce210f8861b"
+}
 
 module "nomad" {
   source  = "app.terraform.io/Darnold-Pipeline/nomad/aws"
   version = "2.0.0"
-
+  bastion_image_id = "${var.hashistack_ami}"
+  consul_image_id = "${var.hashistack_ami}"
   consul_public = "true"
   consul_servers = 3
   name = "${var.cluster_name}"
   nomad_client_docker_install = "true"
   nomad_client_java_install = "true"
+  nomad_image_id = "${var.hashistack_ami}"
   nomad_public = "true"
   nomad_servers = 3
   organization_name = "${var.organization}"
   vault_provision = "true"
   vault_public = "true"
   vault_servers = 3
+  vault_image_id = "${var.hashistack_ami}"
 }
