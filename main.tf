85e98c1dbc2a1e4a3523216ad2e9e8d9ba4cf698
David Arnold <dja+djaboxx@sonic.net>
2018-10-02 21:53:26 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+//--------------------------------------------------------------------
+// Modules
+module "nomad" {
+  source  = "app.terraform.io/Darnold-Pipeline/nomad/aws"
+  version = "1.0.0"
+
+  consul_public = "true"
+  consul_servers = 3
+  name = "${var.cluster_name}"
+  nomad_client_docker_install = "true"
+  nomad_client_java_install = "true"
+  nomad_public = "true"
+  nomad_servers = 3
+  organization_name = "${var.organization}"
+  vault_provision = "true"
+  vault_public = "true"
+  vault_servers = 3
+}
